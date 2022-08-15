f927c86869d385f66922d29b17730b2ff8bc2f76
trenfranco <trenfranco@gmail.com>
2022-08-14 18:21:06 -0700

    ex 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#server config
+
+file_line { 'Identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
+file_line { 'disable password':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
