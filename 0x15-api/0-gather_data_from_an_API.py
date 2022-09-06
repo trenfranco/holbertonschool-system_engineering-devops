9b4c67a6e1564b67e4fa91ccecfa8e9b9f16dccb
trenfranco <trenfranco@gmail.com>
2022-09-06 12:31:37 -0700

    task 0 and readme

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    empId = argv[1]
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(empId))
+    name = response.json().get("name")
+
+    res = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                       .format(empId))
+    ttasks = 0
+    dtasks = 0
+
+    for tasks in res.json():
+        ttasks += 1
+        if tasks.get("completed"):
+            dtasks += 1
+
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(name, dtasks, ttasks))
+    for tasks in res.json():
+        if tasks.get("completed"):
+            print("\t {}".format(tasks.get('title')))
