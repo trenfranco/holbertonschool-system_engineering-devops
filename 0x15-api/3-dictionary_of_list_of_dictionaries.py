309c3b0e6ee17256daeb78b26de74cc7e2adc002
trenfranco <trenfranco@gmail.com>
2022-09-06 19:53:29 -0700

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+
+
+import csv
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    filename = "todo_all_employees.json"
+
+    # response is dict type
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+    # res is list type when passed parameters
+    res = requests.get("https://jsonplaceholder.typicode.com/todos")
+    res = res.json()
+    response = response.json()
+
+    big_dict = {}
+    dic = {}
+    list_dicts = []
+
+    for user in response:
+        list_dicts = []
+        for i in res:
+            dic['username'] = user['username']
+            dic['task'] = i['title']
+            dic['completed'] = i['completed']
+            list_dicts.append(dic)
+        big_dict[user['id']] = list_dicts
+
+
+    with open(filename, 'w') as f:
+        f.write(json.dumps(big_dict))
+        f.close()
