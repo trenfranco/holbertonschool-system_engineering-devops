7c1c478ec6fc70f140d83e3f6a6d34fc7894ec47
trenfranco <trenfranco@gmail.com>
2022-09-21 20:15:18 +0000

    task 0 and readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""from Reddit´sAPI returns number of subscribers for a given sub"""
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """returns number of followers a subReddit has using Reddit API"""
+
+    """identify ourselves"""
+    headers = {'User-Agent': 'MyAPI'}
+
+    res = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit),
+                       headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
