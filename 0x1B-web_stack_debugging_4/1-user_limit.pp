38b08429940fb2485d652eb3bcac0ea7bc52c04a
trenfranco <trenfranco@gmail.com>
2022-09-29 17:47:22 -0700

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,13 @@
-============================ap[sa[
+# now the holberton user can login and open files with no error code
 
--gfh
-'
+# more hard file limit to holb user
+exec { 'hardFileLimitAdded':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
 
-
-'''
-
+# more soft file limit to holb user
+exec { 'softFileLimitAdded':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
